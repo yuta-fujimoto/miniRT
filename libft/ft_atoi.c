6b851beaa6ccc29b0081f7334e2590247a8fdbcf
fyuta <fyuta@student.42tokyo.jp>
2022-01-13 19:46:55 +0900

    replace atoi with rakiyama's one

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,35 +6,53 @@
 /*   By: fyuta <fyuta@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 13:42:51 by fyuta             #+#    #+#             */
-/*   Updated: 2022/01/09 13:27:13 by fyuta            ###   ########.fr       */
+/*   Updated: 2022/01/13 19:46:29 by fyuta            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-bool ft_atoi(const char *nptr, int *rlt)
+static bool	is_overflow(unsigned long num,
+	int sign, long max)
 {
-	int	i;
-	int	minus;
+	if (0 < num && ((sign == 1 && (unsigned long)max < num)
+			|| (sign == -1 && (unsigned long)max < num - 1)))
+		return (true);
+	return (false);
+}
+
+static size_t	pass_isspace(const char *str, size_t i)
+{
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
+		|| str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
+		i++;
+	return (i);
+}
+
+bool	ft_atoi(const char *str, int *rlt)
+{
+	size_t			i;
+	unsigned int	num;
+	int				sign;
 
 	i = 0;
-	*rlt = 0;
-	minus = 1;
-	if (nptr[i] == '-')
+	num = 0;
+	sign = 1;
+	i = pass_isspace(str, i);
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-' && ++i)
+		sign = -1;
+	while (str[i] && ('0' <= str[i] && str[i] <= '9'))
 	{
-		minus *= -1;
+		num = num * 10 + (str[i] - '0');
+		if (is_overflow(num, sign, INT_MAX))
+			break ;
 		i++;
 	}
-	while ('0' <= nptr[i] && nptr[i] <= '9')
-	{
-		if (*rlt > INT_MAX / 10 || (*rlt == INT_MAX / 10
-				&& (nptr[i] - '0') > INT_MAX % 10)
-			|| *rlt < INT_MIN / 10 || (*rlt == INT_MIN / 10
-				&& (nptr[i] - '0') > (INT_MIN % 10) * (-1)))
-			return (false);
-		*rlt = *rlt * 10 + (nptr[i++] - '0') * minus;
-	}
-	if (nptr[i] != '\0')
+	*rlt = (int)num * sign;
+	i = pass_isspace(str, i);
+	if (i == 0 || str[i] != '\0')
 		return (false);
 	return (true);
 }
