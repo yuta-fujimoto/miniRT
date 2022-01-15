6943db09575d7227603472e4c2cfbcb197a69afb
fyuta <fyuta@student.42tokyo.jp>
2022-01-15 11:29:53 +0900

    exit when red cross is pushed

--- tutorial/circle.c
+++ tutorial/circle.c
@@ -40,6 +40,16 @@ bool is_crossed(double x_img, double y_img, t_vecs *vecs)
 		return (false);
 }
 
+int ft_exit(t_data *data)
+{
+	mlx_destroy_image(data->mlx, data->img);
+	mlx_destroy_window(data->mlx, data->mlx_win);
+	mlx_destroy_display(data->mlx);
+	free(data->mlx);
+
+	exit(0);
+}
+
 int	main(void)
 {
 	t_data	data;
@@ -73,6 +83,8 @@ int	main(void)
 		}
 		y_img++;
 	}
+
+	mlx_hook(data.mlx_win, 33, 1 << 17, ft_exit, &data);
 	mlx_put_image_to_window(data.mlx, data.mlx_win, data.img, 0, 0);
 	mlx_loop(data.mlx);
 }
