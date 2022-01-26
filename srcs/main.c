4d76092555275a44fc76108e0fa7e0aa03910dee
fyuta <fyuta@student.42tokyo.jp>
2022-01-26 23:43:01 +0900

    msg

--- srcs/main.c
+++ srcs/main.c
@@ -45,7 +45,7 @@ void	data_init(t_data *data)
 	data->win = isnull(data, \
 						mlx_new_window(data->mlx, \
 							W_IMG, H_IMG, \
-							""));
+							"PRO CAMPER RAKIYAMA"));
 	data->img = isnull(data, \
 						mlx_new_image(data->mlx, W_IMG, H_IMG));
 	data->addr = isnull(data, \
