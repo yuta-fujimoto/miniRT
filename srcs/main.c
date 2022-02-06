5e44527dedc2204260d4013b25f1e8c1dd56ee0b
fyuta <fyuta@student.42tokyo.jp>
2022-02-06 13:34:24 +0900

    redraw

--- srcs/main.c
+++ srcs/main.c
@@ -1,5 +1,18 @@
 #include "miniRT.h"
 
+int redraw(t_data *data)
+{
+	static int cnt = 0;
+
+	cnt++;
+	if (cnt % 10000 == 0)
+	{
+		mlx_put_image_to_window(data->mlx, data->win, data->img, 0, 0);
+		cnt = 0;
+	}
+	return (0);
+}
+
 int	main(int ac, char **av)
 {
 	t_data		data;
@@ -13,6 +26,7 @@ int	main(int ac, char **av)
 	data_set(&data);
 	mlx_hook(data.win, 33, 1 << 17, close_window, &data);
 	mlx_key_hook(data.win, key_hook, &data);
+	mlx_loop_hook(data.mlx, redraw, &data);
 	mlx_put_image_to_window(data.mlx, data.win, data.img, 0, 0);
 	mlx_loop(data.mlx);
 }
