fe5a73deadaa580919513cf6dafc8d3accd9981e
fyuta <fyuta@student.42tokyo.jp>
2022-02-08 22:09:57 +0900

    exposeでできた

--- srcs/init.c
+++ srcs/init.c
@@ -20,7 +20,7 @@ void	data_init(t_data *data)
 	data->win = isnull(data, \
 						mlx_new_window(data->mlx, \
 							W_IMG, H_IMG, \
-							"FYUTA in the house"));
+							"RAKIYAMA:manners maketh man"));
 	data->img = isnull(data, \
 						mlx_new_image(data->mlx, W_IMG, H_IMG));
 	data->addr = isnull(data, \
--- srcs/main.c
+++ srcs/main.c
@@ -2,14 +2,7 @@
 
 int	redraw(t_data *data)
 {
-	static int	cnt = 0;
-
-	cnt++;
-	if (cnt % 10000 == 0)
-	{
-		mlx_put_image_to_window(data->mlx, data->win, data->img, 0, 0);
-		cnt = 0;
-	}
+	mlx_put_image_to_window(data->mlx, data->win, data->img, 0, 0);
 	return (0);
 }
 
@@ -26,7 +19,7 @@ int	main(int ac, char **av)
 	data_set(&data);
 	mlx_hook(data.win, 33, 1 << 17, close_window, &data);
 	mlx_key_hook(data.win, key_hook, &data);
-	mlx_loop_hook(data.mlx, redraw, &data);
+	mlx_expose_hook(data.win, redraw, &data);
 	mlx_put_image_to_window(data.mlx, data.win, data.img, 0, 0);
 	mlx_loop(data.mlx);
 }
